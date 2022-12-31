6dc0529a547b54ef30f38a17d1ad0af6f1fa1879
netox222 <souparnamahato146@gmail.com>
2022-12-31 10:43:16 +0530

    products section adeded

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,28 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Products from './Products';
 
 const Home = () => {
+   const [products , setProducts] = useState([])
+   useEffect(()=>{
+    fetch('http://localhost:5000/products')
+    .then(res => res.json())
+    .then(data => setProducts(data))
+   } , [])
+
+
+
     return (
         <div>
-            <section class="text-gray-600 body-font">
-  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-      <img class="object-cover object-center rounded" alt="hero" src="https://i.ibb.co/yYPsQrv/436e2313-9384-42b5-935c-a926cab32a9d.png"/>
+            <section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img className="object-cover object-center rounded" alt="hero" src="https://i.ibb.co/yYPsQrv/436e2313-9384-42b5-935c-a926cab32a9d.png"/>
     </div>
-    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
-        <br class="hidden lg:inline-block"/>Buy Now
+    <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+        <br className="hidden lg:inline-block"/>Buy Now
       </h1>
-      <p class="mb-8 leading-relaxed">Find your favourite phone here.We are offering best phones at pocket friendly price.So don't think much just buy!</p>
-      <div class="flex justify-center">
-        <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy Now</button>
+      <p className="mb-8 leading-relaxed">Find your favourite phone here.We are offering best phones at pocket friendly price.So don't think much just buy!</p>
+      <div className="flex justify-center">
+        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Buy Now</button>
       </div>
     </div>
   </div>
 </section>
-
-<div>
-    <Products/>
+   <h1 className='text-center text-4xl'>Products</h1>
+<div className='grid md:grid-cols-3 gap-4 md:ml-52 place-content-center '>
+    {products.map(product =>  <Products product={product} key={product._id}/> )}
 </div>
         </div>
     );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,26 +1,21 @@
 import React from 'react';
 
-const Products = () => {
-    return (
-        <div>
-            <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-wrap -m-4">
-      <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-        <a class="block relative h-48 rounded overflow-hidden">
-          <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260"/>
-        </a>
-        <div class="mt-4">
-          <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-          <h2 class="text-gray-900 title-font text-lg font-medium">The Catalyzer</h2>
-          <p class="mt-1">$16.00</p>
-        </div>
-      </div>
-      
+const Products = ({product}) => {
+  const {name , price , image} = product
+  
+  return (
+   
+           <div className=" card  w-64 bg-base-100 shadow-xl">
+  <figure><img className='w-52' src={image} alt={name} /></figure>
+  <div className="">
+    <h2 className="card-title">{name}</h2>
+    <p>${price}</p>
+    <div className="card-actions justify-start">
+      <button className="btn bg-indigo-500 btn-sm">Add to cart</button>
     </div>
   </div>
-</section>
-        </div>
+</div>
+        
     );
 };
 
